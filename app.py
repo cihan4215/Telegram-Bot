b10581594f682d67a18fd94ebaa5c1f95a326342
cihan <chnakbs97@gmail.com>
2025-04-26 21:32:29 +0300

    Updated app.py to add session handling

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def home():
 # ——— Telegram bot ayarları ———
 api_id = 21830452
 api_hash = '2852cd53b42fc0e57bdcf9e29f0ae71e'
-phone = '+923271343743'  # Telefon numarasını buraya sabitleyin
-client = TelegramClient(f'{phone}_session', api_id, api_hash)
+phone = '+923271343743'
+client = TelegramClient(phone, api_id, api_hash)
 
 source_channel_ids = [
     -1002500350398, -1001963997401, -1001662061478, -1001722849883,
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     # 1) Flask’ı arka planda ayağa kaldır
     port = int(os.environ.get('PORT', 8000))
     threading.Thread(
-        target=lambda: app.run(host='0.0.0.0', port=port, use_reloader=False),
+        target=lambda: app.run(host='0.0.0.0', port=port),
         daemon=True
     ).start()
 
